4a8ca6ef73b857b23b81c3958d28d88f6d3f90ac
Artenis67 <artenis67eininger@gmail.com>
2024-02-07 21:07:14 +0100

    Fix des déplacements du joueur et ajustement de sa prefab

--- player.gd
+++ player.gd
@@ -13,9 +13,9 @@ func _process(delta):
 	elif Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 	elif Input.is_action_pressed("move_up"):
-		velocity.y += 1
-	elif Input.is_action_pressed("move_down"):
 		velocity.y -= 1
+	elif Input.is_action_pressed("move_down"):
+		velocity.y += 1
 		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed;
