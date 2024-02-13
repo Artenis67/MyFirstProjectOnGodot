91a7d1c4799f02ce71eccfa798a0b5d8eb586ded
Artenis67 <artenis67eininger@gmail.com>
2024-02-13 23:14:15 +0100

    Update des scripts relatifs au spawn des coins

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -3,8 +3,18 @@ extends Area2D
 @export var main_scene: PackedScene
 var coin_value = 1
 
-func _on_body_entered(body):
-	increase_score(coin_value)
-
-func increase_score(score_):
-	main_scene.score += score_
+func _on_area_entered(area):
+	# Vérifiez si l'objet qui entre en collision a un nom "Player"
+	if area.name == "Player":
+		# Obtenez le noeud principal en instanciant la scène principale
+		var main_node = main_scene.instance()
+		# Vérifiez si le noeud principal existe
+		if main_node != null:
+			# Obtenez le script principal à partir du noeud principal
+			var main_script = main_node.get_script()
+			# Augmenter le score dans le script principal
+			main_script.score += coin_value
+			# Mettre à jour le HUD dans le script principal
+			main_script.increase_score_hud()
+		else:
+			print("Error: Main script node not found")
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,9 +1,11 @@
 extends Node
 
 @export var mob_scene: PackedScene
+@export var coin_scene: PackedScene
 var score
 
 func game_over():
+	$CoinTimer.stop()
 	$Music.stop()
 	$GameOverSound.play()
 	$ScoreTimer.stop()
@@ -11,6 +13,7 @@ func game_over():
 	$HUD.show_game_over()
 
 func new_game():
+	$CoinTimer.start()
 	$Music.play()
 	score = 0
 	$Player.start($StartPosition.position)
@@ -24,6 +27,9 @@ func _on_start_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	increase_score_hud()
+
+func increase_score_hud():
 	$HUD.update_score(score)
 
 func _on_mob_timer_timeout():
@@ -41,3 +47,25 @@ func _on_mob_timer_timeout():
 	
 	add_child(mob)
 
+func _on_coin_timer_timeout():
+	spawn_coin()
+
+func spawn_coin():
+	var newcoin = coin_scene.instantiate()
+	
+	var xpos_ = randf_range(20 ,460)
+	var ypos_ = randf_range(20, 700)
+	var spawn_position = Vector2(xpos_, ypos_)
+	
+	newcoin.position = Vector2(xpos_, ypos_)
+	
+	var coinsparent = get_node("CoinsParent")
+	
+	if coinsparent != null:
+		coinsparent.add_child(newcoin)
+	else:
+		print("Error: coinsparent node not found")
+
+func set_score(new_score):
+	score = new_score
+	$HUD.update_score(score)
